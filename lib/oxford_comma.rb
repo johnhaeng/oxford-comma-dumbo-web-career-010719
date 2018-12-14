810ec4580588810bc48dd6123b92819d5d5e53ff
Jun Haeng Lee <johnhaeng@gmail.com>
2018-12-14 05:54:22 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
-def oxford_comma(array)
-
+def oxford_comma(arr)
+   if arr.length == 1
+    arr.join('')
+  elsif arr.length == 2
+    arr.join(' and ')
+  else arr.length > 2
+    last_element = "and " + arr[-1]
+    arr.pop
+    arr.push(last_element)
+    arr.join(', ')
+  end
 end
